f60d48442db6221784da69d6445aa975627230eb
wyattbodle263 <wyatt.bodle@icloud.com>
2022-12-13 13:56:16 -0800

    Initial Commit

---
+++ Eriks App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eriks App
+//
+//  Created by Wyatt Bodle on 12/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
